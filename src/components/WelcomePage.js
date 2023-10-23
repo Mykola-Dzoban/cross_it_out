d6c3e4adbaea1ac01b170bfd9dc9e8070a758ac5
Mykola Dzoban <nick.dzoban@gmail.com>
2023-10-23 13:36:33 +0300

    added error page, try new toasts

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -9,17 +9,19 @@ const WelcomePage = () => {
 	useEffect(() => {
 		setTimeout(() => {
 			setIsExploding(true);
-			setIsStart(true)
+			setIsStart(true);
 		}, 4500);
 	}, []);
 	return (
 		<div className="h-[50vh] w-full flex items-center justify-center flex-col gap-5 transition-all duration-300">
 			<div className="welcome-block border-slate-500">
 				<span className="welcome-text">C</span>
-				<div className="welcome-cross bg-red-900"></div>
+				<div className="welcome-cross bg-red-700"></div>
 				{isExploding && <ConfettiExplosion duration={2000} className="absolute" />}
 			</div>
-			{isStart && <Link to='/main' className="btn btn-primary">Start</Link>}
+			<Link to="/main" className={`btn btn-primary ${isStart ? 'flex' : 'hidden'}`}>
+				Start
+			</Link>
 		</div>
 	);
 };
--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import { Provider } from 'react-redux';
 import { App } from './App';
 import './index.css';
 import store from './store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<Provider store={store}>
 		<App />
+		<ToastContainer position="top-center" autoClose={2000} />
 	</Provider>
 );
