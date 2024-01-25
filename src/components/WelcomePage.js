367e594638e0356807df5e3d062b23af849fec0d
Mykola Dzoban <nick.dzoban@gmail.com>
2024-01-25 15:24:01 +0200

    changing design using perkslab-ui library

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,4 +1,5 @@
 import { LogIn } from 'lucide-react';
+import { Button } from 'perkslab-ui';
 import { useEffect, useState } from 'react';
 import ConfettiExplosion from 'react-confetti-explosion';
 import { useDispatch, useSelector } from 'react-redux';
@@ -24,12 +25,41 @@ const WelcomePage = () => {
 	}, []);
 	return (
 		<div className="h-[50vh] w-full flex items-center justify-center flex-col gap-5 transition-all duration-300">
-			<div className="welcome-block border-slate-500">
-				<span className="welcome-text">C</span>
+			<div className="welcome-block border-teal-700">
+				<span className="welcome-text leading-[0] select-none">C</span>
 				<div className="welcome-cross bg-red-700"></div>
 				{isExploding && <ConfettiExplosion duration={2000} className="absolute" />}
 			</div>
-			<button
+			<Button
+				type="primary"
+				className={` items-center gap-2 ${isStart ? 'flex' : 'hidden'}`}
+				onClick={async () => {
+					if (isAuth) {
+						navigate('/main');
+					} else {
+						try {
+							const user = await users.loginWithGoogle();
+							dispatch(login());
+							localStorage.setItem('userId', JSON.stringify(user.id));
+							navigate('/main');
+						} catch (error) {
+							console.error('Error during Google login:', error);
+						}
+					}
+				}}>
+				{isAuth ? (
+					<>
+						<LogIn />
+						Enter
+					</>
+				) : (
+					<>
+						<LogIn />
+						Google
+					</>
+				)}
+			</Button>
+			{/* <button
 				className={`btn btn-primary font-bold ${isStart ? 'flex' : 'hidden'}`}
 				onClick={async () => {
 					if (isAuth) {
@@ -54,7 +84,7 @@ const WelcomePage = () => {
 						<LogIn /> Google
 					</>
 				)}
-			</button>
+			</button> */}
 		</div>
 	);
 };
